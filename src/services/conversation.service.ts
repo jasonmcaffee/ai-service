fb4ac8e4ebbe7fde65cb90483781e8943c360268
Jason <jasonlmcaffee@gmail.com>
2025-05-13 17:54:53 -0600

    filter out certain messages when naming the conversation.

--- src/services/conversation.service.ts
+++ src/services/conversation.service.ts
@@ -124,6 +124,7 @@ export class ConversationService {
     const prompt = nameConversationPrompt(conversation);
 
     let openAiMessages = createOpenAIMessagesFromMessages(conversation.messages || []);
+    openAiMessages = openAiMessages.filter(m => m.role !== "tool"); //filter out tool calls
     const lastMessage = {role: 'user', content: prompt} as ChatCompletionMessageParam;
     openAiMessages = [...openAiMessages, lastMessage];
     const result = await this.inferenceService.nonStreamingInference(openAiMessages);
